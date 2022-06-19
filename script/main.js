22dbb3061c90f288ccc5e72ff643f5c6c450a5d0
Nataliahu97 <nataliahu97@gmail.com>
2022-06-19 09:32:49 -0700

    guardar repo

---
+++ script/main.js
@@ -0,0 +1,20 @@
+
+//Este es un comentario en linea
+
+/*Este es 
+un comentario 
+en bloque*/
+
+//var name = "natalia";
+//var phone = 3235042199;
+//var rh = "A+"
+//let name = "Natalia";
+
+//const pi = 3.1416;
+
+//console.log("Hola" + name);
+
+//name = "John";
+
+//tips de datos
+//number 
